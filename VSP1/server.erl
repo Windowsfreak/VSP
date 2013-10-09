d71f0261cb1001191ef9b8c9c758aec24c584434
sk <sk@sk.com>
2013-10-09 18:54:27 +0200

    server queue management

--- VSP1/server.erl
+++ VSP1/server.erl
@@ -14,7 +14,7 @@ loop(HBQ, DLQ, Clients, NextNnr, ServerCfg) ->
 			ClientPID ! {nnr, NextNnr},
 			loop(HBQ, DLQ, Clients, NextNnr + 1, ServerCfg)
 	end.
-
+ 
 	
 queueInsert(Queue, Message) ->
 	NewQueue = werkzeug:pushSL(Queue, Message),
@@ -32,3 +32,41 @@ queueCrop(Queue, MaxLength) ->
 	
 dlqInsert(DLQ, Message, MaxLength) ->
 	queueCrop(queueInsert(DLQ, Message), MaxLength).
+	
+hbqInsert(HBQ, Message, DLQ, MaxLength, DLQLength) ->
+	{Nnr, _Msg} = Message,
+	MaxDLQ = werkzeug:maxNrSL(DLQ),
+	if Nnr > MaxDLQ ->
+		NewHBQ = queueInsert(HBQ,Message),
+		hbqClean(NewHBQ, DLQ, MaxLength, DLQLength);
+	true ->
+		[HBQ,DLQ]
+	end.
+	
+hbqClean(HBQ, DLQ, MaxLength, DLQLength) ->
+	[NewHBQ, NewDLQ] = hbqCrop(HBQ, DLQ, DLQLength),
+	Length = werkzeug:lengthSL(NewHBQ),
+	if Length > MaxLength ->
+		MinHBQ = werkzeug:minNrSL(NewHBQ),
+		hbqClean(NewHBQ, dlqInsert(NewDLQ, {MinHBQ - 1, "Fehlernachricht für eine Lücke"}, DLQLength), MaxLength, DLQLength);
+	true ->
+		[NewHBQ, NewDLQ]
+	end.
+	
+hbqCrop(HBQ, DLQ, DLQLength) ->
+	MinHBQ = werkzeug:minNrSL(HBQ),
+	MaxDLQ = werkzeug:maxNrSL(DLQ),
+	if MaxDLQ + 1 == MinHBQ ->
+		Elem = werkzeug:findSL(HBQ, MinHBQ),
+		hbqCrop(werkzeug:popSL(HBQ), dlqInsert(DLQ, Elem, DLQLength), DLQLength);
+	true -> 
+		[HBQ, DLQ]
+	end.
+		
+	
+	
+	
+	
+	
+	
+	
