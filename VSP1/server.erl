b0a96c4905947fd6953b2c017260df5338be7fc2
sk <sk@sk.com>
2013-10-09 17:45:38 +0200

    server testing

--- VSP1/server.erl
+++ VSP1/server.erl
@@ -1,20 +1,34 @@
 -module(server).
 -compile([export_all]).
 
-{ok, ServerCfg} = file:consult("server.cfg"),
-% {ok, MaxLength} = werkzeug:get_config_value(maxLength, ServerCfg),
 
+start() -> spawn(server, loop, []).
+
+loop() ->
+	{ok, ServerCfg} = file:consult("server.cfg"),
+	%{ok, MaxLength} = werkzeug:get_config_value(maxLength, ServerCfg),
+	loop(0,0,0,0,0). 
 loop(HBQ, DLQ, Clients, NextNnr, ServerCfg) ->
-	true. % TODO
+	receive
+		{getmsgid, ClientPID} -> 
+			ClientPID ! {nnr, NextNnr},
+			loop(HBQ, DLQ, Clients, NextNnr + 1, ServerCfg)
+	end.
+
 	
 queueInsert(Queue, Message) ->
 	NewQueue = werkzeug:pushSL(Queue, Message),
 	NewQueue.
 	
-queueCrop(Queue, MaxLength) when werkzeug:lengthSL(Queue) > MaxLength ->
-	queueCrop(popSL(Queue), MaxLength);
+	
 queueCrop(Queue, MaxLength) ->
-	Queue.
+	Length = werkzeug:lengthSL(Queue),
+	if Length > MaxLength ->
+		queueCrop(werkzeug:popSL(Queue), MaxLength);
+	true -> 
+		Queue
+	end.
+
 	
 dlqInsert(DLQ, Message, MaxLength) ->
 	queueCrop(queueInsert(DLQ, Message), MaxLength).
