5930468b66cf23459b25ac0cfb2133e25143af50
Björn Eberhardt <git@bjoern>
2013-10-09 13:05:32 +0200

    Zweiter Commit

---
+++ VSP1/client.erl
@@ -0,0 +1,29 @@
+-module(client).
+-compile([export_all]).
+
+loop() ->
+	true. % TODO
+
+sendMsgs() ->
+    true. % TODO
+
+receiveMsgs() ->
+    true. % TODO
+
+randomizeDelay(Delay) ->
+	max(1, Delay * (random:uniform(2) * 2 - 1) / 2.0). % force float
+
+requestNnr(ServerPID, NnrList) ->
+	Nnr = requestNnr(ServerPID),
+    [Nnr, lists:append(NnrList, [Nnr])].
+
+requestNnr(ServerPID) ->
+	ServerPID ! {getmsgid, self()},
+	receive
+		{nnr, Number} -> Number
+	end. % TODO
+
+isKnown
+	(_Nnr, []) -> false;
+	(Nnr, [Nnr | _List) -> true;
+	(Nnr, [_Nnr | List) -> isKnown(Nnr, List);
